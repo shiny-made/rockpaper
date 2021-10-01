048d5d98ee3be8948702760587c967ac70a92f86
shiny-made <maqsudmallick2.0@gmail.com>
2021-10-01 11:39:57 -0700

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+
+
+function App() {
+  return (
+    <div>
+         Score
+        Rules
+
+        You Picked
+        The House Picked
+
+        You Win
+        You Lose
+
+        Play Again
+    </div>
+  );
+}
+
+export default App;
